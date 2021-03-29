7a4e9d35ac99f91019a1e66b5d0c872d4abe67c2
Wyatt Phillips <phillipsw1@wit.edu>
2021-03-29 08:54:03 -0400

    Initialize peripheral definitions

---
+++ zoom.c
@@ -0,0 +1,8 @@
+#include "address_map.h"
+
+volatile short* pixel_buffer = (short*)PIXEL_BUFFER_BASE;
+volatile short* char_buffer = (short*)CHARACTER_BUFFER_BASE;
+
+int main(void) {
+	return 0;
+}
